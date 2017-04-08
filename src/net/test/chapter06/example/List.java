df5d1324dd6aeb08b13e61b57facf866bff01477
wuli2496 <wuli2496@163.com>
2017-04-08 09:54:19 +0800

    修改目录结构及包名冲突的解决

---
+++ src/net/test/chapter06/example/List.java
@@ -0,0 +1,11 @@
+package net.test.chapter06.example;
+
+/**
+ * Created by John on 2017/4/8.
+ */
+public class List {
+    public List()
+    {
+        System.out.println("net.test.chapter06.example.List");
+    }
+}
---
+++ src/net/test/chapter06/example/Vector.java
@@ -0,0 +1,11 @@
+package net.test.chapter06.example;
+
+/**
+ * Created by John on 2017/4/8.
+ */
+public class Vector {
+    public Vector()
+    {
+        System.out.println("net.test.chapter06.example.Vector");
+    }
+}
